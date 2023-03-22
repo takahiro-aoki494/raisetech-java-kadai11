33de4e9457d00630b927c6825800338a4f1f2ed5
takahiro-aoki494 <arikatuki@gmail.com>
2023-03-22 21:52:09 +0900

    プロジェクトのセットアップ

---
+++ src/test/java/raisetech/todoapi/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
